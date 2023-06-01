72baa1c08253ac8b4ba875ac40663208c4b584b7
SantiW01 <sweihmuller01@gmail.com>
2023-06-01 14:55:41 -0300

    Set access modifiers to private and add getter and setter

--- src/app/Calculator/calculator.component.ts
+++ src/app/Calculator/calculator.component.ts
@@ -5,7 +5,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./calcualtor.component.css'],
 })
 export class CalculatorComponent {
-  number1: number = 0;
-  number2: number = 0;
-  operator: string = '';
+  private number1: number = 0;
+  private number2: number = 0;
+  private operator: string = '';
+
+  setNumber1(number1: number): void {
+    this.number1 = number1;
+  }
+
+  getNumber1(): number {
+    return this.number1;
+  }
+
+  setNumber2(number2: number): void {
+    this.number2 = number2;
+  }
+
+  getNumber2(): number {
+    return this.number2;
+  }
+
+  setOperator(operator: string): void {
+    this.operator = operator;
+  }
+
+  getOperator(): string {
+    return this.operator;
+  }
 }
